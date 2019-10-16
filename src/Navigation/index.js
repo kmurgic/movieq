4f0018537baa10eec64157ef17602addd3dc480f
Keith Murgic <keith.murgic@brandpulse.io>
2019-10-16 17:33:19 -0500

    Collapse nav bar on link selection

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleSelect = () => {
+    setExpanded(false);
+  }
 
   return (
-    <Navbar bg="main" variant="dark" expand="lg" fixed="top">
+    <Navbar
+      bg="main"
+      variant="dark"
+      expand="lg"
+      fixed="top"
+      expanded={expanded}
+      onToggle={() => setExpanded(prev => !prev)}
+    >
       <Navbar.Brand as={NavLink} to="/">React-Bootstrap</Navbar.Brand>
       <Navbar.Toggle aria-controls="main-navbar-nav" />
       <Navbar.Collapse id="main-navbar-nav">
         <Nav className="ml-auto mr-sm-2">
-          <Nav.Link as={NavLink} to="discover">Discover</Nav.Link>
-          <Nav.Link as={NavLink} to="queues">My Queues</Nav.Link>
-          <Nav.Link as={NavLink} to="profile">My Profile</Nav.Link>
+          <Nav.Link
+            onClick={handleSelect}
+            as={NavLink}
+            to="discover"
+          >
+            Discover
+          </Nav.Link>
+          <Nav.Link
+            onClick={handleSelect}
+            as={NavLink}
+            to="queues"
+          >
+            My Queues
+          </Nav.Link>
+          <Nav.Link
+            onClick={handleSelect}
+            as={NavLink}
+            to="profile"
+          >
+            My Profile
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
--- src/Navigation/tests/index.test.js
+++ src/Navigation/tests/index.test.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Navigation from '../index';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-it('renders without crashing', () => {
-  shallow(<Navigation />);
+describe('Navigation', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Navigation />);
+  });
+  it('renders without crashing', () => {
+    expect(wrapper);
+  });
+  it('toggles on toggle click', () => {
+    const navbar = wrapper.find(Navbar);
+    navbar.invoke('onToggle')();
+    const newNavbar = wrapper.find(Navbar);
+    expect(newNavbar.props().expanded).toBe(true);
+  });
+  it('collapses on nav link click', () => {
+    const navbar = wrapper.find(Navbar);
+    navbar.invoke('onToggle')();;
+    const navLink = wrapper.find(Nav.Link).at(0);
+    navLink.invoke('onClick')();
+    const newNavbar = wrapper.find(Navbar);
+    expect(newNavbar.props().expanded).toBe(false);
+  });
 });
+
+
+
