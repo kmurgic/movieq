6bab2559dd2d9310444b6f4042d03d34fa75a4bd
Keith Murgic <keith.murgic@brandpulse.io>
2019-10-16 19:07:39 -0500

    Added search bar to discover page

--- src/Discover/index.js
+++ src/Discover/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import classes from './index.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 const Discover = () => {
   return (
-    <h2>Search for movies to add to your queues.</h2>
-  )
-}
+    <>
+      <Form className="d-flex justify-content-center" inline>
+        <FormControl
+          type="text"
+          placeholder="Search for Movies to Add to Your Queue"
+          className={`${classes['search-input']} w-75 mr-0`}
+        />
+        <Button
+          className={classes['search-button']}
+          variant="outline-success"
+        >
+          <FontAwesomeIcon icon={faSearch} />
+        </Button>
+      </Form>
+    </>
+  );
+};
 
 export default Discover;
