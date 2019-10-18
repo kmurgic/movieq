9ae32d58ee0451c5d0da5231b9b1dd5358467158
Keith Murgic <keith.murgic@brandpulse.io>
2019-10-18 15:18:10 -0500

    Added search actions to actions folder

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,12 @@
 import { FETCH_MOVIES_REQUEST } from './types';
+import { QUERY_MOVIES_REQUEST } from './types';
+
 
 export const topMoviesRequest = () => ({
   type: FETCH_MOVIES_REQUEST,
+});
+
+export const searchMoviesRequest = (searchTerm) => ({
+  type: QUERY_MOVIES_REQUEST,
+  payload: searchTerm,
 });
