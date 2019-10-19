37524cae3f9511158490fb9c50eb0bbebad9f711
Keith Murgic <keith.murgic@brandpulse.io>
2019-10-18 21:45:50 -0500

    Added keys to list items in queue

--- src/pages/Queues/index.js
+++ src/pages/Queues/index.js
@@ -5,7 +5,11 @@ import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
-const movies = ['Movie 1 (2019)', 'Movie 2 (2017)', 'Movie 3 (2015)'];
+const movies = [
+  { id: 'm1', title: 'Movie 1 (2019)' },
+  { id: 'm2', title: 'Movie 2 (2017)' },
+  { id: 'm3', title: 'Movie 3 (2015)' },
+];
 
 const Queues = () => {
   return (
@@ -19,6 +23,7 @@ const Queues = () => {
               const variant = index % 2 ? 'info' : 'light';
               return (
                 <ListGroup.Item
+                  key={movie.id}
                   action
                   className={`${classes['queue-item']} d-flex justify-content-between`
                     + ' align-items-center pl-sm-0 pt-sm-0 pb-sm-0'}
@@ -28,7 +33,7 @@ const Queues = () => {
                     src="https://image.tmdb.org/t/p/w370_and_h556_bestv2/a4BfxRK8dBgbQqbRxPs8kmLd8LG.jpg"
                     thumbnail
                   />
-                  <span className={classes['movie-title']}>{movie}</span>
+                  <span className={classes['movie-title']}>{movie.title}</span>
                   <Button size='lg' variant="outline-danger">
                     <FontAwesomeIcon icon={faTrashAlt} />
                   </Button>
