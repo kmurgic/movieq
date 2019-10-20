436abeeddfed98135d09b41971ea2de074c81915
Keith Murgic <keith.murgic@brandpulse.io>
2019-10-20 15:41:11 -0500

    Added form to discover page

---
+++ src/pages/Discover/selectOptions.js
@@ -0,0 +1,29 @@
+export const genres = [
+  "All",
+  "Action",
+  "Adventure",
+  "Animation",
+  "Comedy",
+  "Crime",
+  "Documentary",
+  "Drama",
+  "Family",
+  "Fantasy",
+  "History",
+  "Horror",
+  "Music",
+  "Mystery",
+  "Romance",
+  "Science Fiction",
+  "TV Movie",
+  "Thriller",
+  "War",
+  "Western"
+];
+
+export const ratings = [
+  'G',
+  'PG',
+  'PG-13',
+  'R',
+];
