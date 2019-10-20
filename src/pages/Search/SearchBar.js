8fe863892b7729a7e5cbb862ec8a779e02b203ff
Keith Murgic <keith.murgic@brandpulse.io>
2019-10-20 09:12:17 -0500

    Use input group for search bar to improve styling

--- src/pages/Search/SearchBar.js
+++ src/pages/Search/SearchBar.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import classes from './index.module.css';
@@ -22,20 +23,24 @@ const SearchBar = props => {
       inline
       onSubmit={handleSubmit}
     >
-      <FormControl
-        type="text"
-        placeholder="Search for Movies..."
-        className={`${classes['search-input']} w-50 mr-0`}
-        onChange={(e) => setQuery(e.target.value)}
-        value={query}
-      />
-      <Button
-        className={classes['search-button']}
-        type="submit"
-        variant="outline-secondary"
-      >
-        <FontAwesomeIcon icon={faSearch} onClick={handleSearch} />
-      </Button>
+      <InputGroup className="flex-nowrap">
+        <FormControl
+          type="text"
+          placeholder="Search for Movies..."
+          className={`${classes['search-input']} w-50 mr-0`}
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+        />
+        <InputGroup.Append>
+          <Button
+            className={classes['search-button']}
+            type="submit"
+            variant="outline-secondary"
+          >
+            <FontAwesomeIcon icon={faSearch} onClick={handleSearch} />
+          </Button>
+        </InputGroup.Append>
+      </InputGroup>
     </Form>
   );
 }
